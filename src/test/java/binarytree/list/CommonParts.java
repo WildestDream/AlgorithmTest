7ca39c6c5e39b31959be1dfe6cf1936f02382181
xiaofei_global <xiaofei_global@163.com>
2022-01-17 22:22:58 +0800

    【链表】相关练习

---
+++ src/test/java/binarytree/list/CommonParts.java
@@ -0,0 +1,44 @@
+package binarytree.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 现有两个升序链表，且链表中均无重复元素。请设计一个高效的算法，打印两个链表的公共值部分。
+ *
+ * 给定两个链表的头指针headA和headB，请返回一个vector，元素为两个链表的公共部分。请保证返回数组的升序。两个链表的元素个数均小于等于500。保证一定有公共值
+ *
+ * 测试样例：
+ * {1,2,3,4,5,6,7},{2,4,6,8,10}
+ * 返回：[2.4.6]
+ */
+public class CommonParts {
+    public int[] findCommonParts(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode l1 = headA;
+        ListNode l2 = headB;
+        List<Integer> list = new ArrayList<>();
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                l1 = l1.next;
+            } else if (l1.val > l2.val) {
+                l2 = l2.next;
+            } else {
+                list.add(l1.val);
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+        }
+        return buildResult(list);
+    }
+
+    private int[] buildResult(List<Integer> list) {
+        int[] res = new int[list.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+}
---
+++ src/test/java/binarytree/list/KInverse.java
@@ -0,0 +1,51 @@
+package binarytree.list;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+/**
+ * 有一个单链表，请设计一个算法，使得每K个节点之间逆序，如果最后不够K个节点一组，则不调整最后几个节点。
+ * 例如链表1->2->3->4->5->6->7->8->null，K=3这个例子。调整后为，3->2->1->6->5->4->7->8->null。
+ * 因为K==3，所以每三个节点之间逆序，但其中的7，8不调整，因为只有两个节点不够一组。
+ *
+ * 给定一个单链表的头指针head,同时给定K值，返回逆序后的链表的头指针。
+ */
+public class KInverse {
+
+    @Test
+    public void test() {
+        //0,1,2,5,11 2
+        ListNode n1 = new ListNode(0);
+        ListNode n2 = new ListNode(1);
+        ListNode n3 = new ListNode(2);
+        ListNode n4 = new ListNode(5);
+        ListNode n5 = new ListNode(11);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        ListNode inverse = inverse(n1, 2);
+        System.out.println(inverse);
+    }
+
+    public ListNode inverse(ListNode head, int k) {
+        if (head == null) {
+            return null;
+        }
+        Stack<Integer> stack = new Stack<>();
+        ListNode p1 = head;
+        ListNode p2 = head;
+        while (p1 != null) {
+            stack.push(p1.val);
+            if (stack.size() == k) {
+                while (!stack.isEmpty()) {
+                    p2.val = stack.pop();
+                    p2 = p2.next;
+                }
+            }
+            p1 = p1.next;
+        }
+        return head;
+    }
+}
